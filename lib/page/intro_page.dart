ccfea52a389d4bded896f8f6bd10502ac89f7fb1
faqihyugos <faqihyugos@gmail.com>
2023-07-19 12:24:12 +0700

    route

---
+++ lib/page/intro_page.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
+
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
