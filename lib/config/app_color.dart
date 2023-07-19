99f45225d49bdf35884ed953728cae1c4eefd710
faqihyugos <faqihyugos@gmail.com>
2023-07-19 12:11:36 +0700

    update color

--- lib/config/app_color.dart
+++ lib/config/app_color.dart
@@ -5,7 +5,6 @@ class AppColor {
   static const secondary = Color(0xff123E56);
   static const backgroundScaffold = Color(0xffF6F6F6);
 
-  static const starActive = Color(0xffFA8F29;
-);
+  static const starActive = Color(0xffFA8F29);
   static const starInActive = Color(0xffF6F6F6);
 }
